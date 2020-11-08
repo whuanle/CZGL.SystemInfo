6c53edefc3254f39efd4e9aa8993d2614bd2c107
痴者工良 <yzf@whuanle.cn>
2020-11-08 21:59:07 +0800

    删除一点东西

--- examples/Consoletest/Program.cs
+++ examples/Consoletest/Program.cs
@@ -19,17 +19,7 @@ namespace Consoletest
     {
         public static void Main()
         {
-            var z = (NetworkInterface.GetAllNetworkInterfaces()
-                                                          .Where(x =>
-                                                          x.NetworkInterfaceType != NetworkInterfaceType.Loopback
-                                              && x.NetworkInterfaceType != NetworkInterfaceType.Ethernet)).ToList();
-            Console.WriteLine(z.Count);
-            foreach (var item in z)
-            {
-                Console.WriteLine(item.OperationalStatus);
-            }
-
-
+            Console.WriteLine(NetworkInfo.GetPhysicalMac);
             Console.ReadKey();
         }
     }
--- src/CZGL.SystemInfo.Linux/DynamicInfo.cs
+++ src/CZGL.SystemInfo.Linux/DynamicInfo.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.InteropServices;
-using static CZGL.SystemInfo.TollHelper;
 
 namespace CZGL.SystemInfo.Linux
 {
