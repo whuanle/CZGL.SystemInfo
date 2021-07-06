b3504e70df2d6d57035b433865e6394da4a1d49e
痴者工良 <yzf@whuanle.cn>
2021-07-06 23:41:38 +0800

    增加winform示例

---
+++ examples/WindowsFormsApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using CZGL.ProcessMetrics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            new Thread(() =>
+            {
+                MetricsServer metricsServer = new MetricsServer("http://*:1234/metrics/");
+                metricsServer.Start();
+            }).Start();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
