2a8103a6df3e0217e2d8cfaf05f316fb00f09c82
痴者工良 <yzf@whuanle.cn>
2021-07-06 21:18:52 +0800

    修复了 https://github.com/whuanle/CZGL.SystemInfo/issues/5 问题

--- src/CZGL.ProcessMetrics/Counters/EventSourceCreatedListener.cs
+++ src/CZGL.ProcessMetrics/Counters/EventSourceCreatedListener.cs
@@ -4,10 +4,30 @@ using System.Diagnostics.Tracing;
 
 namespace CZGL.ProcessMetrics.Counters
 {
+    /// <summary>
+    /// 监听 CLR 中的所有事件
+    /// </summary>
     sealed class EventSourceCreatedListener : EventListener
     {
         protected override void OnEventSourceCreated(EventSource source)
         {
+            /*
+             * CLR 中包含以下监视器，但是这里只使用 System.Runtime
+             * Microsoft-Windows-DotNETRuntime
+             * System.Runtime
+             * Microsoft-System-Net-Http
+             * System.Diagnostics.Eventing.FrameworkEventSource
+             * Microsoft-Diagnostics-DiagnosticSource
+             * Microsoft-System-Net-Sockets
+             * Microsoft-System-Net-NameResolution
+             * System.Threading.Tasks.TplEventSource
+             * System.Buffers.ArrayPoolEventSource
+             * Microsoft-System-Net-Security
+             * System.Collections.Concurrent.ConcurrentCollectionsEventSource
+             */
+            if (!source.Name.Equals("System.Runtime"))
+                return;
+
             EnableEvents(source, EventLevel.Verbose, EventKeywords.All, new Dictionary<string, string>()
             {
                 ["EventCounterIntervalSec"] = "1"
@@ -25,47 +45,70 @@ namespace CZGL.ProcessMetrics.Counters
             }
         }
 
+        // 解析其中一个值生成:
+        // dotnet_xxx{label="value"} value
         private static void GetRelevantMetric(
             IDictionary<string, object> eventPayload)
         {
             if (!eventPayload.TryGetValue("Name", out object name)) return;
-            if (eventPayload.TryGetValue("DisplayName", out object displayValue)) { }
+            if (!eventPayload.TryGetValue("DisplayName", out object displayValue))
+            {
+                displayValue = string.Empty;
+            }
 
             var processMetrics = ProcessMetricsCore.Instance;
-            var gauge = processMetrics.CreateGauge("dotnet_" + name.ToString().Replace("-", "_"), displayValue.ToString());
+            var gauge = processMetrics
+                .CreateGauge("dotnet_" + name.ToString().Replace("-", "_")
+                , displayValue.ToString());
             var labels = gauge.Create();
 
+            // 只要能够取得两者之一的值即可
             if (eventPayload.TryGetValue("Mean", out object value) ||
                 eventPayload.TryGetValue("Increment", out value))
             {
-                var v = decimal.Parse(value.ToString());
+                if (!decimal.TryParse(value.ToString(), out var v))
+                {
+                    v = 0;
+                }
 
-                if (eventPayload.TryGetValue("DisplayUnits", out var units)) { };
+                // 获取计量单位
+                if (!eventPayload.TryGetValue("DisplayUnits", out var units))
+                {
+                    labels.AddValue(v);
+                    return;
+                }
 
 
-                var parseValue = Parse(v,units.ToString());
+                var parseValue = ParseUnits(v, units.ToString());
                 labels.AddLabel("display_units", parseValue.units);
                 labels.AddValue(parseValue.value);
             }
         }
 
-        private static (decimal value, string units) Parse(object value, string units)
+        /// <summary>
+        /// 转换计量单位。不会损失精确度
+        /// 例如 72.6MB，变成 72.6*1024*1024=76,126,617.6 字节，在 Grafana 中最终也会换成 MB
+        /// </summary>
+        /// <param name="objectValue"></param>
+        /// <param name="units"></param>
+        /// <returns></returns>
+        private static (decimal value, string units) ParseUnits(object objectValue, string units)
         {
-            if (!int.TryParse(value.ToString(), out var v)) return (0, "");
+            if (!long.TryParse(objectValue.ToString(), out var v)) return (0, string.Empty);
 
-            if (units == null)
-                return (decimal.Parse(value.ToString()), units);
+            if (units == null || units == string.Empty)
+                return (v, units);
 
             if (units.Equals("KB", StringComparison.OrdinalIgnoreCase))
-                return ((decimal)(v << 10), "B");
+                return ((v << 10), "B");
 
             if (units.Equals("MB", StringComparison.OrdinalIgnoreCase))
-                return ((decimal)(v << 20), "B");
+                return ((v << 20), "B");
 
             if (units.Equals("GB", StringComparison.OrdinalIgnoreCase))
-                return ((decimal)(v << 30), "B");
+                return ((v << 30), "B");
 
-            return (decimal.Parse(value.ToString()), units);
+            return (v, units);
         }
     }
 
